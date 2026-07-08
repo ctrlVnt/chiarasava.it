488b2c00c1786bbd0b238d187e0baa223edee8fb
chiare774 <chiara.sava97@gmail.com>
2026-07-08 12:33:08 +0200

    Update Research.tsx

--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -62,8 +62,8 @@ const Research = () => {
       authors: [
         { name: "C. Sava", link: "#" }
       ],
-      journal: "Algebras and Representation Theory (2026) 29:565–607",
-      year: "#",
+      journal: "Algebras and Representation Theory",
+      year: "(2026)",
       link: "https://doi.org/10.1007/s10468-026-10388-3"
     }
   ];
